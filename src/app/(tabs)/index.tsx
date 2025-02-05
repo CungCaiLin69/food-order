bf4ea71a68c1256d8c5a0d1432937e8cab8409d6
CungCaiLin69 <sendy.adriansyah17@gmail.com>
2025-02-05 16:27:33 +0700

    added list and column

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,33 +1,15 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
 import products from '@/assets/data/products';
 import ProductListItem from '@/src/components/ProductListItem';
+import { View, FlatList } from 'react-native';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    <FlatList 
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />} 
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 10
-  },
-  price: {
-    color: Colors.light.tint,
-  }
-});
